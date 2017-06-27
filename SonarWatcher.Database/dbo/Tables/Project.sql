2437a7b5cc3973e73a0094f46e12893200dd5135
hedo <hedojunior@gmail.com>
2017-06-27 17:06:50 -0300

    remover coluna extra da tabela person e adiciona-la na tabela project

--- SonarWatcher.Database/dbo/Tables/Project.sql
+++ SonarWatcher.Database/dbo/Tables/Project.sql
@@ -2,6 +2,7 @@
     [Id]       INT           IDENTITY (1, 1) NOT NULL,
     [Name]     VARCHAR (150) NOT NULL,
     [SonarKey] VARCHAR (150) NULL,
+    [Active] INT NULL, 
     CONSTRAINT [PK_Project] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
